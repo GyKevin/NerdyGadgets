17bda3ae68b783137db851bfaa2c13d72f0c669d
Joppiesaus2349 <Jopscholten@gmail.com>
2023-10-17 13:49:52 +0200

    Revert "..."
    This reverts commit 43ee5bb35a37b022a11cec0b81fbc18a126a0779.

--- pages/product.php
+++ pages/product.php
@@ -43,67 +43,13 @@
     <main>
         <!-- all the data gets imported by doing '<?php //echo content ?>' 
         feel free to move things around, as long as you include this.  -->
-        <div class="Image_container">
-            <img src="../image/product_images/<?php echo $productImage ?>.jpg" alt="product image" width="100px">
-        </div>
-        <div class="all_info">
-            <h2><?php echo $productName ?></h2>
-            <h4>€ <?php echo $productPrice ?></h4>
-            <div class="add_to_cart">
-                <div class="quantity">
-                    <span>Add Quantity</span>
-                    <input type="number" name="qty" id="number" value="1" />
-                </div>
-                <button class="buy_now" tabindex="0">
-                    <i class="fa fa-shopping-cart"></i> Buy Now
-                </button>
-                <button class="add_cart" tabindex="0">
-                    <i class="fa fa-shopping-cart"></i> Add to Cart
-                </button>
-            </div>
-            <div class="Product_beschrijving">
-                <h3>Product beschrijving</h3>
-                <p><?php echo $productDescription ?></p>
-
-            </div>
-        </div>
+        <img src="../image/product_images/<?php echo $productImage ?>.jpg" alt="product image" width="100px">
+        <p><?php echo $productName ?></p>
+        <p>€ <?php echo $productPrice ?></p>
+        <p><?php echo $productDescription ?></p>
     </main>
 
     <!-- footer -->
     <div id="footer"></div>
 </body>
 </html>
-<style>
-    main{
-        display: flex;
-    }
-    .Image_container {
-        display: flex;
-    }
-    img{
-        width: 400px;
-        height: auto;
-        margin: 100px;
-        object-fit: contain;
-    }
-    .add_cart{
-        margin-top: 20px;
-        margin-left: 0px;
-        margin-bottom: 15px;
-        height: 30px;
-        width: 125px;
-        background-color: #4CAF50; /* Green */
-        border: none;
-        color: white;
-        padding: 15px 32px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-
-    }
-    .Product_beschrijving{
-    }
-    
-
-</style>
