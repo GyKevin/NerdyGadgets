1bc2db740617d12e5456dd90442ffd7ab09ab3ba
Joppiesaus2349 <Jopscholten@gmail.com>
2023-11-01 09:44:01 +0100

    Login en registratie volledig

--- pages/Login.php
+++ pages/Login.php
@@ -23,21 +23,30 @@
 <div id="navbar"></div>
 
 <!-- content -->
+<div class="login_content">
     <div class="container">
         <div class="login-form">
             <h2>Inloggen</h2>
             <form action="login.php" method="post">
+                
                 <input type="text" name="username" placeholder="Gebruikersnaam" required>
                 <input type="password" name="password" placeholder="Wachtwoord" required>
                 <button class="Inloggen" type="submit">Inloggen</button>
             </form>
         </div>
+    </div>
+</div>
     <!-- footer -->
     <div id="footer"></div>
 </body>
 </html>
 <style>
-    body {
+.login_content {
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+    }
+body {
     font-family: Arial, sans-serif;
     background-color: #ffffff;
     color: #333;
@@ -45,12 +54,32 @@
     justify-content: center;
     margin: 0;
 }
+.container {
+    display: flex;
+    background-color: #ffffff;
+    border: 1px solid #ddd;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    overflow: hidden;
+    margin: 100px;
+    width: 400px;
+    justify-content: center;
+    
+}
+.container > div {
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
 
 s
 .login-form {
     background-color: #ffffff;
     text-align: center;
     border-right: 1px solid #ddd;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
 }
 
 h2 {
@@ -79,6 +108,7 @@ input {
     background-color: #23232f;
     color: #ffffff;
     margin-top: 10px;
+    margin-bottom: 10px;
     border: none;
     border-radius: 3px;
     cursor: pointer;
--- pages/Registratie.php
+++ pages/Registratie.php
@@ -23,7 +23,9 @@
 <div id="navbar"></div>
 
 <!-- content -->
-<div class="register-form">
+<div class="registeer_content">
+<div class="container">
+    <div class="register-form">
             <h2>Registreren</h2>
             <form action="register.php" method="post">
                 <input type="email" name="email" placeholder="E-mailadres" required>
@@ -31,12 +33,19 @@
                 <input type="password" name="password" placeholder="Wachtwoord" required>
                 <button class="Registreer" type="submit">Registreren</button>
             </form>
-        </div>
+    </div>
+</div>
+</div>
 <!-- footer -->
     <div id="footer"></div>
 </body>
 <style>
-    body {
+.registeer_content{
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+    }
+body {
     font-family: Arial, sans-serif;
     background-color: #ffffff;
     color: #333;
@@ -52,6 +61,8 @@
     border-radius: 5px;
     overflow: hidden;
     margin: 100px;
+    width: 450px;
+    justify-content: center;
     
 }
 .container > div {
@@ -61,20 +72,27 @@
     align-items: center;
     justify-content: center;
 }
+
+s
 .register-form {
     background-color: #ffffff;
     text-align: center;
+    border-right: 1px solid #ddd;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
 }
+
 h2 {
     color: #333;
     font-size: 24px;
     margin-bottom: 20px;
 }
+
 form {
     width: 100%;
     max-width: 300px;
     margin-right: 20px;
 }
+
 input {
     width: 100%;
     padding: 12px;
@@ -82,16 +100,20 @@ input {
     border: 1px solid #ddd;
     border-radius: 3px;
 }
-.Registreer{
+
+.Registreer {
     width: 108%;
     padding: 12px;
     background-color: #23232f;
     color: #ffffff;
+    margin-top: 10px;
+    margin-bottom: 10px;
     border: none;
     border-radius: 3px;
     cursor: pointer;
-    margin-top: 10px;
+    
 }
+
 button:hover {
     background-color: #4e4e58;
 }
