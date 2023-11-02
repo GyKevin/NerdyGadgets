f70148c970fc386e244285aeae20ca875e560be7
Braduptempo <ryan.b.a@live.nl>
2023-11-02 21:05:37 +0100

    algemene-voorwaardes.php aangemaakt

---
+++ pages/algemene-voorwaardes.php
@@ -0,0 +1,21 @@
+<html lang="nl">
+<header>
+    <link rel="stylesheet" href="/navbar/navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="/navbar/import-handler.js"></script>
+    <style>
+        body {
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+        }
+        #footer {
+            margin-top: auto;
+        }
+    </style>
+</header>
+<body>
+
+</body>
+
+</html>
