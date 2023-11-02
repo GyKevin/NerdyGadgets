1d19af238786b34af91dfb2bf7afb81cf8b7832b
Ryan Asikende <ryan.asikende@hva.nl>
2023-11-02 21:32:57 +0100

    algemene-voorwaarden.php bewerkt met de voorwaardes. algemene-voorwaarden.css aangemaakt voor de spacing.

---
+++ pages/algemene-voorwaarden.php
@@ -0,0 +1,111 @@
+<html lang="nl">
+<header>
+    <link rel="stylesheet" href="/navbar/navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/algemene-voorwaarden.css">
+    <script src="/navbar/import-handler.js"></script>
+    <style>
+        body {
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+        }
+        #footer {
+            margin-top: auto;
+        }
+    </style>
+</header>
+<body>
+<!-- navigatie bar -->
+<div id="navbar"></div>
+<!-- content -->
+<main>
+    <div class="algemene-voorwaarden">
+        <br>
+        <br>
+        <h1>Algemene Voorwaarden voor NerdyGadgets</h1>
+        <p>Laatst bijgewerkt: 2-11-2023</p>
+        <h2>1. Algemeen</h2>
+        <p>
+            1.1. Deze algemene voorwaarden zijn van toepassing op alle overeenkomsten tussen NerdyGadgets (hierna te noemen
+            "Verkoper," "wij" of "ons") en de klant (hierna te noemen "Klant" of "u") met betrekking tot de aankoop en
+            verkoop van producten via de website van NerdyGadgets.
+        </p>
+        <p>
+            1.2. Door het plaatsen van een bestelling op NerdyGadgets
+            verklaart u akkoord te gaan met en gebonden te zijn aan deze algemene voorwaarden.
+        </p>
+
+        <h2>2. Productinformatie en Prijsstelling</h2>
+        <p>
+            2.1. Wij streven ernaar om accurate productinformatie te verstrekken, inclusief beschrijvingen, prijzen en
+            afbeeldingen. Echter, wij kunnen geen garantie geven voor de absolute nauwkeurigheid van deze informatie.
+        </p>
+        <p>
+            2.2. Prijzen kunnen zonder voorafgaande kennisgeving worden gewijzigd. Alle prijzen zijn inclusief BTW en
+            andere toepasselijke belastingen.
+        </p>
+
+        <h2>3. Bestellen en Betalen</h2>
+        <p>
+            3.1. Door het plaatsen van een bestelling gaat u ermee akkoord om de totale kosten van de bestelling te
+            betalen, inclusief de prijs van de producten, verzendkosten en eventuele belastingen.
+        </p>
+        <p>
+            3.2. Betalingen kunnen worden gedaan via de beschikbare betaalmethoden op de website van NerdyGadgets.
+        </p>
+        <p>
+            3.3. Bestellingen zijn pas definitief nadat de betaling is ontvangen en bevestigd.
+        </p>
+        <h2>4. Verzending en Levering</h2>
+        <p>
+            4.1. Wij streven ernaar om bestellingen zo snel mogelijk te verzenden. De geschatte levertijden zijn indicatief
+            en kunnen variëren.
+        </p>
+        <p>
+            4.2. De verzendkosten worden weergegeven tijdens het bestelproces en kunnen variëren op basis van de
+            bezorg bestemming en de grootte/gewicht van de bestelling.
+        </p>
+        <h2>5. Retourbeleid</h2>
+        <p>
+            5.1. U heeft het recht om uw aankoop binnen 14 dagen na ontvangst te retourneren, op voorwaarde dat het product
+            in ongebruikte staat verkeert en in de originele verpakking zit.
+        </p>
+        <p>
+            5.2. Voor retourzendingen moet u contact opnemen met onze klantenservice om instructies te ontvangen.
+        </p>
+        <h2>6. Privacybeleid</h2>
+        <p>
+            6.1. Uw persoonlijke gegevens worden verwerkt in overeenstemming met ons Privacybeleid, dat beschikbaar is op
+            onze website.
+        </p>
+        <h2>7. Aansprakelijkheid en Garantie</h2>
+        <p>
+            7.1. Wij streven ernaar om kwaliteitsproducten te leveren. Eventuele garanties worden expliciet vermeld in de
+            productinformatie.
+        </p>
+        <p>
+            7.2. Onze aansprakelijkheid is beperkt tot het aankoopbedrag van het product.
+        </p>
+        <h2>8. Toepasselijk recht en Geschillen</h2>
+        <p>
+            8.1. Op deze algemene voorwaarden is Nederlands recht van toepassing.
+        </p>
+        <p>
+            8.2. Geschillen die voortvloeien uit of verband houden met deze overeenkomst zullen in eerste instantie worden
+            beslecht door middel van onderhandeling. Indien geen minnelijke schikking wordt bereikt, zal het geschil worden
+            voorgelegd aan de bevoegde Nederlandse rechtbank.
+        </p>
+        <h2>9. Wijzigingen in de Algemene Voorwaarden</h2>
+        <p>
+            9.1. NerdyGadgets behoudt zich het recht voor om deze algemene voorwaarden op elk moment te wijzigen.
+            Gewijzigde voorwaarden zijn van toepassing op nieuwe bestellingen.
+        </p>
+    </div>
+</main>
+<!-- footer -->
+<div id="footer"></div>
+
+</body>
+
+</html>
