3e0bb82facac015ea403027647e011e95806f391
Joppiesaus2349 <Jopscholten@gmail.com>
2023-11-03 09:11:44 +0100

    Winkelwagen start

---
+++ pages/winkelwagen.php
@@ -0,0 +1,77 @@
+<link rel="stylesheet" href="/navbar/navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="/navbar/import-handler.js"></script>
+    <style>
+        body {
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+        }
+        main{
+            padding-top: 55px;
+        }
+        #footer {
+            margin-top: auto;
+        }
+    </style>
+    <body>
+    <!-- navigatie bar -->
+    <div id="navbar"></div>
+    <!-- content -->
+    <main>
+    <div class="content">
+        <div class="winkelwagen">
+<!-- "product 1" -->
+            <div class="item">
+                <div class="buttons">
+                    <button>Delete</button>
+                </div>
+            </div>
+
+            <div class="image">
+                <img src="...">
+            </div>
+
+            <div class="description">
+                <p>uufbvuubb nincionoiwn incwioenje</p>
+                <p>jnvjnfojdnof kicninceroi cpewniewn</p>
+            </div>
+
+            <div class="quantity"> 
+                <input type="number" name="quantity" value="1">
+            </div>
+
+        <div class="price">
+            <p>$10</p>
+        </div>
+        </div>
+    </div>
+<!-- product 2 -->
+<div class="item">
+        <div class="buttons">
+            <button>Delete</button>
+        </div>
+            </div>
+
+            <div class="image">
+                <img src="...">
+            </div>
+
+            <div class="description">
+                <p>uufbvuubb nincionoiwn incwioenje</p>
+                <p>jnvjnfojdnof kicninceroi cpewniewn</p>
+            </div>
+
+            <div class="quantity"> 
+                <input type="number" name="quantity" value="1">
+            </div>
+
+        <div class="price">
+            <p>$10</p>
+        </div>
+        </div>
+    </div>
+    </main>
+    <!-- footer -->
+    <div id="footer"></div>
+    </body>
