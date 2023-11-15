28e6f4cc1e21cc5edf079c1583b7cdabc2120b1c
Joppiesaus2349 <Jopscholten@gmail.com>
2023-11-15 14:44:30 +0100

    Winkelwagentje beetje gefixt.
    Alleen nog die titels die gefixt moeten worden

--- pages/winkelwagen.php
+++ pages/winkelwagen.php
@@ -1,4 +1,10 @@
-<link rel="stylesheet" href="/navbar/navbar.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Winkelwagen</title>
+    <link rel="stylesheet" href="/navbar/navbar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="/navbar/import-handler.js"></script>
     <link rel="stylesheet" href="/css/winkelwagen.css">
@@ -15,6 +21,7 @@
             margin-top: auto;
         }
     </style>
+</head>
     <body>
     <!-- navigatie bar -->
     <div id="navbar"></div>
@@ -25,10 +32,11 @@
             <div class="titel">
                 Winkelwagen
             </div>
+            
 <!-- "product 1" -->
         <div class="item">
             <div class="buttons">
-            <button><img src="/image/111056_trash_can_icon.png"></button> 
+            <button type="button"><img src="/image/111056_trash_can_icon.png"></button> 
             </div>
 
             <div class="image">
@@ -47,9 +55,25 @@
             <div class="price">
                 <p>$10</p>
             </div>
+         </div>
+
+        </div>
+        <div class="overzicht">
+            <div class="overzicht_titel">
+               Overzicht
+            </div>
+
+            <div class="totaal">
+                <p>Subtotaal $</p> 
+                <p>Shipping free</p>
+                <p>Taxes $</p>
+                <p>Totaal</p> 
+            </div>
+            <div class="checkout_button">
+                <button>Checkout</button>
+            </div>
+        </div>
     </div>
-</div>
-</div>
     </main>
     <!-- footer -->
     <div id="footer"></div>
