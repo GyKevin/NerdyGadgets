6bcf3d0e4b4fa119be4d5e01f73ac3e20aaad516
Kevin <kvindso@gmail.com>
2023-11-15 21:25:14 +0100

    Update changePass.php

--- api/changePass.php
+++ api/changePass.php
@@ -12,22 +12,6 @@ if ($res->num_rows > 0) {
         $newPass = $_POST['new_password'];
         $newPass2 = $_POST['again_password'];
 
-        //check if old password is correct
-        // if ($oldPass == $currPass && $newPass == $newPass2 && !empty($oldPass) && !empty($newPass) && !empty($newPass2)){
-        //     $updatePass = "UPDATE user SET password = '$newPass' WHERE id = " . $_SESSION['user_id'];
-        //     $_SESSION['success'] = "Uw wachtwoord is succesvol geupdate";
-        //     header("location: ../pages/changePass.php");
-        // } elseif ($oldPass != $currPass) {
-        //     $_SESSION['error'] = "Oude w achtwoorden matchen niet";
-        //     header("location: ../pages/changePass.php");
-        // } elseif ($newPass != $currPass) {
-        //     $_SESSION["error"] = "Nieuwe wachtwoorden matchen niet";
-        //     header("location: ../pages/changePass.php");
-        // } elseif (empty($oldPass) || empty($newPass) || empty($newPass2)) {
-        //     $_SESSION["error"] = "Vul alle velden in";
-        //     header("location: ../pages/changePass.php");
-        // }
-
         $error_count = 0;
         //check if old password matches with the one in the input field
         if ($oldPass != $currPass) {
